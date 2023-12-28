f6eafaead655671e271713443278a04987787a2c
Muhammadi2 <mshislam@gmail.com>
2023-12-28 14:34:51 -0500

    add edit functionality to the profile page with keeping listing option as commented with useeffect

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,15 +1,35 @@
 import React from 'react'
-import { useNavigate } from 'react-router-dom';
 import { useState } from "react";
-import { getAuth } from 'firebase/auth';
+import { getAuth, updateProfile } from "firebase/auth";
+import {
+  collection,
+  deleteDoc,
+  doc,
+  getDocs,
+  orderBy,
+  query,
+  updateDoc,
+  where,
+} from "firebase/firestore";
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { db } from "../firebase";
+import { FcHome } from "react-icons/fc";
+import { useEffect } from "react";
+import ListingItem from "../components/ListingItem";
+
 function Profile() {
   const auth = getAuth();
   const navigate = useNavigate();
+  const [changeDetail, setChangeDetail] = useState(false)
+  const [listings, setListings] = useState(null);
+  const [loading, setLoading] = useState(true);
+
   const [formData, setFormData] = useState( {
     name: auth.currentUser.name,
-
     email:auth.currentUser.email,
   });
+
   const {name, email} = formData;
 
   function onLogout(){
@@ -17,30 +37,103 @@ function Profile() {
     navigate("/")
 
   }
+  function onChange(e) {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  }
+
+  async function onSubmit() {
+    try {
+      if (auth.currentUser.displayName !== name) {
+        //update display name in firebase auth
+        await updateProfile(auth.currentUser, {
+          displayName: name,
+        });
+
+        // update name in the firestore
+        // First need to create docstore with collection name users.
+
+        const docRef = doc(db, "users", auth.currentUser.uid);
+        await updateDoc(docRef, {
+          name,
+        });
+      }
+      toast.success("Profile details updated");
+    } catch (error) {
+      toast.error("Could not update the profile details");
+    }
+  }
+  // useEffect(() => {
+  //   async function fetchUserListings() {
+  //     const listingRef = collection(db, "listings");
+  //     const q = query(
+  //       listingRef,
+  //       where("userRef", "==", auth.currentUser.uid),
+  //       orderBy("timestamp", "desc")
+  //     );
+  //     const querySnap = await getDocs(q);
+  //     let listings = [];
+  //     querySnap.forEach((doc) => {
+  //       return listings.push({
+  //         id: doc.id,
+  //         data: doc.data(),
+  //       });
+  //     });
+  //     setListings(listings);
+  //     setLoading(false);
+  //   }
+  //   fetchUserListings();
+  // }, [auth.currentUser.uid]);
+  // async function onDelete(listingID) {
+  //   if (window.confirm("Are you sure you want to delete?")) {
+  //     await deleteDoc(doc(db, "listings", listingID));
+  //     const updatedListings = listings.filter(
+  //       (listing) => listing.id !== listingID
+  //     );
+  //     setListings(updatedListings);
+  //     toast.success("Successfully deleted the listing");
+  //   }
+  // }
+  // function onEdit(listingID) {
+  //   navigate(`/edit-listing/${listingID}`);
+  // }
+
   return (
+    <>
     <section className="max-w-6xl mx-auto flex justify-center items-center flex-col ">
       <h1 className="text-center text-3xl mt-6 font-bold"> My Profile</h1>
       <div className="w-full md:w-[50%] mt-6 px-3">
         <form>
-          <input type="text" value={name} disabled className="w-full px-4 py-2 text-x1 text-gray-700
-           bg-white border border-gray-300 rounded transition ease-in-out mb-6" />
+          <input type="text" 
+          value={name} 
+          disabled={changeDetail}
+          onChange={onChange}
+          // if the changgDetai is true then chang ethe backgron color to red
+          className={`mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out ${
+            changeDetail && "bg-red-200 focus:bg-red-200"
+          }`} />
            
            {/* Email Input */}
-          <input type="email" id ="email" value={email} disabled className="w-full px-4 py-2 text-x1 text-gray-700
+          <input type="email" id ="email" value={email} disabled 
+          
+          className="w-full px-4 py-2 text-x1 text-gray-700
            bg-white border border-gray-300 rounded transition ease-in-out" />
            <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg mb-6" >
            <p className="flex items-center">
 
            Do you want to change your name?
            <span
-                  // onClick={() => {
-                  //   changeDetail && onSubmit();
-                  //   setChangeDetail((prevState) => !prevState);
-                  // }}
+                  onClick={() => {
+                      // if change detail is true the submit th eform
+                    changeDetail && onSubmit();
+                    setChangeDetail((prevState) => !prevState);
+                  }}
                   className="text-red-600 hover:text-red-700 transition ease-in-out duration-200 ml-1 cursor-pointer"
                 >
-                  {/* {changeDetail ? "Apply change" : "Edit"} */}
-                   Edit
+                  {changeDetail ? "Apply change" : "Edit"}
+    
                 </span>
 
            </p> 
@@ -55,6 +148,7 @@ function Profile() {
         </form>
       </div>
     </section>
+    </>
 );
 }
 export default Profile
